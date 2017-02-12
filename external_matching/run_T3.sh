955b947abf1eb69c7d7afa43bb70793effb65db4
Philipp Windischhofer (ETHZ) [phwindis] <philipp.windischhofer@gmail.com>
2017-02-12 21:09:55 +0100

    started running

--- external_matching/run_T3.sh
+++ external_matching/run_T3.sh
@@ -10,8 +10,8 @@ export MKL_NUM_THREADS=1
 export THEANO_FLAGS="base_compiledir=$HOME/theano.NOBACKUP"
 
 # for local testing
-JOB_ID=testing
-JOB_DESC="pure test"
+JOB_ID=LSTM64_1layer_large_training
+JOB_DESC="trains a 64/1 LSTM with all track parameters, 150000 as training set length, 100 epochs"
 
 # for saving the output
 OUTDIR=$HOME/DeepTagger/RNN_out_external_matching/${JOB_ID}
@@ -22,17 +22,17 @@ echo $JOB_DESC > $OUTDIR/desc.txt
 
 # to specify the model topology and the jet / track parameters accessible to the network
 # number of nodes per layer
-NODES=32
-LAYERS=2
-EPOCHS=10
-TRAINING_LENGTH=1000
+NODES=64
+LAYERS=1
+EPOCHS=100
+TRAINING_LENGTH=150000
 
 # number of tracks (-1 ... use all that are available)
-TRACKS=10
+TRACKS=-1
 
 # which track parameters are passed on to the network (as their indices)
 # [ "Track_pt", "Track_eta", "Track_phi", "Track_dxy", "Track_dz", "Track_IP", "Track_IP2D", "Track_length" ]
-PARAMETERS="0 1 2 3 4 5"
+PARAMETERS="0 1 2 3 4 5 6 7"
 
 echo "starting"
 
