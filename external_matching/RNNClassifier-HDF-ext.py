5a2aad8580bbec1e7c34df1b7f0c5cd1f9dbc46a
Philipp Windischhofer (ETHZ) [phwindis] <philipp.windischhofer@gmail.com>
2017-03-23 19:23:07 +0100

    generator implemented and working

--- external_matching/RNNClassifier-HDF-ext.py
+++ external_matching/RNNClassifier-HDF-ext.py
@@ -9,8 +9,10 @@ from keras.models import Sequential, load_model
 from keras.optimizers import SGD
 from keras.layers import Dense, Activation, Dropout
 from keras.layers import LSTM
+from keras.callbacks import EarlyStopping
 from h5IO import *
 from parse_arguments import *
+import threading
 
 # build here the keras model
 def RNN_classifier(nodes, layers, input_dimension):
@@ -43,14 +45,46 @@ def create_truth_output(raw_data):
 
     return y_train
 
-def datagen(path, length, number_tracks, jet_parameters_requested, tracks_requested):
-    raw_data = pd.read_hdf(path, start = 0, stop = length)
-    x_train = create_track_list(raw_data, number_tracks, jet_parameters_requested, tracks_requested, ordered = True)
-    y_train = create_truth_output(raw_data)
-    print(x_train.shape)
-    print(y_train.shape)
+# to make generators thread-safe
+class ts_gen:
+    def __init__(self, gen):
+        self.gen = gen
+        self.lock = threading.Lock()
 
-    return (x_train, y_train)
+    def __iter__(self):
+        return self
+
+    def next(self):
+        with self.lock:
+            return self.gen.next()
+
+def peek_nrows(path):
+    store = pd.HDFStore(path)
+    retval = store.get_storer('data').nrows
+    store.close()
+    return retval    
+
+def datagen(path, length, number_tracks, jet_parameters_requested, tracks_requested):
+
+    act_pos = 0
+    end_pos = length
+    nrows = peek_nrows(path)
+
+    while True:
+        # read the next chunk from the datafile
+        raw_data = pd.read_hdf(path, start = act_pos, stop = end_pos)
+
+        # wrap-around
+        act_pos = end_pos
+        end_pos += length
+        if(end_pos > nrows):
+            act_pos = 0
+            end_pos = length
+
+        x_train = create_track_list(raw_data, number_tracks, jet_parameters_requested, tracks_requested, ordered = True)
+        y_train = create_truth_output(raw_data)
+
+        yield x_train, y_train
 
 def main(argv):
 
@@ -65,9 +99,10 @@ def main(argv):
     number_epochs = args['number_epochs']
     training_dataset_length = args['training_length']
 
-    #datafile = '/shome/phwindis/data/matched/1.h5'
-    datafile = '/scratch/snx3000/phwindis/matched/1.h5'
-    datafile_val = '/scratch/snx3000/phwindis/matched/3.h5'
+    datafile = '/shome/phwindis/data/matched/1.h5'
+    datafile_val = '/shome/phwindis/data/matched/3.h5'
+    #datafile = '/scratch/snx3000/phwindis/matched/1.h5'
+    #datafile_val = '/scratch/snx3000/phwindis/matched/3.h5'
 
     # read in training data
     with pd.HDFStore(datafile) as store:
@@ -83,27 +118,18 @@ def main(argv):
 
     print("max number of tracks is " + str(number_tracks))
 
-    print("reading training data")
-    #raw_data = pd.read_hdf(datafile, start = 0, stop = training_dataset_length)
-
     # create model
     model = RNN_classifier(args['number_nodes'], args['number_layers'], len(jet_parameters_requested))
 
-    # build training input and output:
-    #x_train = create_track_list(raw_data, number_tracks, jet_parameters_requested, tracks_requested, ordered = True)
-    #y_train = create_truth_output(raw_data)
+    # generators that read chunks of data from the training / validation dataset
+    training_gen = ts_gen(datagen(datafile, 10000, number_tracks, jet_parameters_requested, tracks_requested))
+    validation_gen = ts_gen(datagen(datafile_val, 10000, number_tracks, jet_parameters_requested, tracks_requested))
 
-    #print(x_train.shape)
-    #print(y_train.shape)
+    # set up the callback for early stopping of the training
+    early_stop = EarlyStopping(monitor = 'val_loss', patience = 10, verbose = 1, mode = 'auto')
 
     print("start training")
-    #history = model.fit(x_train, y_train, validation_split = 0.20, nb_epoch = number_epochs, batch_size = 1000, shuffle = True)
-    history = model.fit_generator(datagen(datafile, 1000, number_tracks, jet_parameters_requested, tracks_requested),
-                                  samples_per_epoch = 10,
-                                  nb_epoch = 10,
-                                  verbose = 2,
-                                  validation_data = datagen(datafile_val, 1000, number_tracks, jet_parameters_requested, tracks_requested),
-                                  nb_val_samples = 10)
+    history = model.fit_generator(generator = training_gen, samples_per_epoch = 100000, nb_epoch = 10, validation_data = validation_gen, nb_val_samples = 10000, nb_worker = 1, callbacks = [early_stop])
 
     # process the training and validation loss histories
     fig = plt.figure(figsize=(10,6))
